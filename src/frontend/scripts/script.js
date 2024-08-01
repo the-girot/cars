99e8802640bb9b152e31c5bb4a3cc77059e1aa53
the-girot <suvorov.girot@gmail.com>
2024-08-01 16:53:03 +0300

    second gen

--- src/frontend/scripts/script.js
+++ src/frontend/scripts/script.js
@@ -14,10 +14,42 @@ function decrement() {
 function loadContent() {
   try{
     document.getElementById("que_label").innerText = states__[step].que_label
-    document.getElementById("btn question").innerText = states__[step].btn_question
+    document.getElementById("btn_question").innerText = states__[step].btn_question
     document.getElementById("buttons").innerHTML = states__[step].buttons}
     catch(err){}
 }
+
+
+function savestate(el_id) {
+    const table = document.getElementById(el_id);
+    if (table){
+    const tableHtml = table.innerHTML;
+    localStorage.setItem(el_id, tableHtml);
+    }
+    
+}
+
+function loadstate(el_id) {
+    const savedTableHtml = localStorage.getItem(el_id);
+    const table_content = document.getElementById(el_id);
+    if (savedTableHtml && table_content) {
+        table_content.innerHTML = savedTableHtml; // Update table content with saved HTML
+    }
+
+    const tbody = table_content.querySelector('tbody'); // Select the tbody element inside the table
+
+    tbody.addEventListener('click', function(event) {
+        if (event.target.classList.contains('delete-btn')) {
+            event.target.closest('tr').remove(); // Delete the closest row when the button is clicked
+        }
+    });
+}
+
+function clearstate(el_id) {
+    localStorage.removeItem(el_id);
+}
+
+
 
 
 const search_field = document.getElementById("search-field question");
@@ -45,6 +77,6 @@ catch (err) {}
 list_items.forEach((list_item) => {
     list_item.addEventListener("click", () => {
         console.log(list_item)
-        document.getElementById("btn question").textContent = list_item.textContent;
+        document.getElementById("btn_question").textContent = list_item.textContent;
     });
 });
--- src/frontend/scripts/states.js
+++ src/frontend/scripts/states.js
@@ -1,56 +1,65 @@
 const states__ = [
   {
+    "param": ["region",],
     "que_label":"Выберите регион",
     "btn_question":"Регион",
-    "buttons":'<button type="button" class="btn w-100" onclick="increment(); loadContent()" style="background-color: #2887fa; color: #fff">Начать</button>' 
+    "buttons":'<button type="button" class="btn w-100 form_button" onclick="increment(); loadContent()" >Начать</button>' 
     
   },
   {
+    "param": ["brand",],
     "que_label":"Выберите марку",
     "btn_question":"Марка",
-    "buttons":'<button type="button" class="btn w-100" onclick="decrement(); loadContent()" style="background-color: #2887fa; color: #fff">Назад</button>' 
-    +'<button type="button" class="btn w-100" onclick="increment(); loadContent()" style="background-color: #2887fa; color: #fff">Далее</button>'
+    "buttons":'<button type="button" class="btn w-100 form_button" onclick="decrement(); loadContent()" >Назад</button>' 
+    +'<button type="button" class="btn w-100 form_button" onclick="increment(); loadContent()" >Далее</button>'
   },
   {
+    "param": ["model",],
     "que_label":"Выберите модель",
     "btn_question":"Модель",
-    "buttons":'<button type="button" class="btn w-100" onclick="decrement(); loadContent()" style="background-color: #2887fa; color: #fff">Назад</button>' 
-    +'<button type="button" class="btn w-100" onclick="increment(); loadContent()" style="background-color: #2887fa; color: #fff">Далее</button>'
+    "buttons":'<button type="button" class="btn w-100 form_button" onclick="decrement(); loadContent()" >Назад</button>' 
+    +'<button type="button" class="btn w-100 form_button" onclick="increment(); loadContent()" >Далее</button>'
   },
   {
+    "param": ["creationYear",],
     "que_label":"Выберите дату производства",
     "btn_question":"Дата производства",
-    "buttons":'<button type="button" class="btn w-100" onclick="decrement(); loadContent()" style="background-color: #2887fa; color: #fff">Назад</button>' 
-    +'<button type="button" class="btn w-100" onclick="increment(); loadContent()" style="background-color: #2887fa; color: #fff">Далее</button>'
+    "buttons":'<button type="button" class="btn w-100 form_button" onclick="decrement(); loadContent()" >Назад</button>' 
+    +'<button type="button" class="btn w-100 form_button" onclick="increment(); loadContent()" >Далее</button>'
   },
   {
+    "param": ["generation",],
     "que_label":"Выберите поколение",
     "btn_question":"Поколение",
-    "buttons":'<button type="button" class="btn w-100" onclick="decrement(); loadContent()" style="background-color: #2887fa; color: #fff">Назад</button>' 
-    +'<button type="button" class="btn w-100" onclick="increment(); loadContent()" style="background-color: #2887fa; color: #fff">Далее</button>'
+    "buttons":'<button type="button" class="btn w-100 form_button" onclick="decrement(); loadContent()" >Назад</button>' 
+    +'<button type="button" class="btn w-100 form_button" onclick="increment(); loadContent()" >Далее</button>'
   },
   {
+    "param": ["body",],
     "que_label":"Выберите тип кузова",
     "btn_question":"Кузов",
-    "buttons":'<button type="button" class="btn w-100" onclick="decrement(); loadContent()" style="background-color: #2887fa; color: #fff">Назад</button>' 
-    +'<button type="button" class="btn w-100" onclick="increment(); loadContent()" style="background-color: #2887fa; color: #fff">Далее</button>'
+    "buttons":'<button type="button" class="btn w-100 form_button" onclick="decrement(); loadContent()" >Назад</button>' 
+    +'<button type="button" class="btn w-100 form_button" onclick="increment(); loadContent()" >Далее</button>'
   },
   {
+    "param": ["gear",],
     "que_label":"Выберите КПП",
     "btn_question":"КПП",
-    "buttons":'<button type="button" class="btn w-100" onclick="decrement(); loadContent()" style="background-color: #2887fa; color: #fff">Назад</button>' 
-    +'<button type="button" class="btn w-100" onclick="increment(); loadContent()" style="background-color: #2887fa; color: #fff">Далее</button>'
+    "buttons":'<button type="button" class="btn w-100 form_button" onclick="decrement(); loadContent()" >Назад</button>' 
+    +'<button type="button" class="btn w-100 form_button" onclick="increment(); loadContent()" >Далее</button>'
   },
   {
+    "param": ["drive",],
     "que_label":"Выберите тип привода",
     "btn_question":"Привод",
-    "buttons":'<button type="button" class="btn w-100" onclick="decrement(); loadContent()" style="background-color: #2887fa; color: #fff">Назад</button>' 
-    +'<button type="button" class="btn w-100" onclick="increment(); loadContent()" style="background-color: #2887fa; color: #fff">Далее</button>'
+    "buttons":'<button type="button" class="btn w-100 form_button" onclick="decrement(); loadContent()" >Назад</button>' 
+    +'<button type="button" class="btn w-100 form_button" onclick="increment(); loadContent()" >Далее</button>'
   },
   {
+    "param": ["engine",],
     "que_label":"Выберите двигатель",
     "btn_question":"Двигатель",
-    "buttons":'<button type="button" class="btn w-100" onclick="decrement(); loadContent()" style="background-color: #2887fa; color: #fff">Оценить бесплатно</button>'
+    "buttons":'<button type="button" class="btn w-100 form_button" onclick="decrement(); loadContent()" >Оценить бесплатно</button>'
   },
 
 
